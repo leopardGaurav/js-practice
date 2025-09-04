7fd3774c8e56177dc5ac45ad7deaebf82fb493b4
gaurav kumar <gauravkumar20843@gmail.com>
2025-09-04 12:20:35 +0530

    Update conversion.js

--- conversion.js
+++ conversion.js
@@ -1,8 +1,53 @@
-let score = true
-console.log(typeof(score))
+// let score = "33"
+// console.log(typeof(score))//string
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))//number
+
+
+// let score = "33abc"
+// console.log(typeof(score))//string
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))//number
+
+// let score = null
+// console.log(typeof(score))//object
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))//number
+// console.log(valueInNumber)// 0
+
+
+let score = undefined
+console.log(typeof(score))//object
 
 let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))//number
+console.log(valueInNumber)// 0
 
-console.log(typeof valueInNumber);
+// let score = true
+// console.log(typeof(score))
 
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //false
+
+// let isLoggedIn = "ram"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //true
+
+
+// let someNumber = 33
+// let stringNumber  = String(someNumber)
+// console.log(stringNumber);//33
+// console.log(typeof(stringNumber));//string
+
