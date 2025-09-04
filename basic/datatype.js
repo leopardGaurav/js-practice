2acec7b87b9dcc5d62a3da7862b9a836598d9804
gaurav kumar <gauravkumar20843@gmail.com>
2025-09-04 21:21:45 +0530

    Create datatype.js

---
+++ basic/datatype.js
@@ -0,0 +1,36 @@
+//datatype: it mean type/category of data
+//          it define which type of data store in variable
+//two type: 1. primitive --> type --> 7 type
+//          2. non-primitive --> type--> 3 type
+// 1. primitive --> all primitivetype are call by value
+//call by value :always change it's copy value , original value arn't change
+//  type --> 7 type
+// 1.string
+// 2.number
+// 3.boolean
+// 4.null    ---> value is empty not a zero
+// 5.undefined
+// 6.symbol
+// 7.BigInt
+// 2. Reference/non-primitive --> type--> 3 type
+//1.Array
+//const heros = ["sghaktiman","naagraj","doga"]
+
+//2.Objects
+/*let myObj{
+name:"gaurav",
+age:20,
+} 
+isko ham variable me declare kar sakte hain
+ya direct bhi ham  object ko {} ke ander likh sakte hain*/
+//3.Functions
+/** 
+ function ko bhi ham variable me store kar sakte hain
+ direct bhi likh sakte hain
+ const myFunction = function(){
+ console.log("hello")
+ }
+ */
+
+ //all non-primitive type  return in term of object
+  //all non-primitive datatype  return in term of object
