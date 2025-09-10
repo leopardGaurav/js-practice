8a7b966bad607315333cc8d7273abc2d2949a612
gaurav kumar <gauravkumar20843@gmail.com>
2025-09-10 23:35:30 +0530

    Create number.js

---
+++ basic/number.js
@@ -0,0 +1,54 @@
+// const score = 400
+// console.log(score);
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3))//23.9
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3))//124
+// const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(3))//1.12e+3
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++++math+++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));//4
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2));//5
+// console.log(Math.floor(4.9));//4
+// console.log(Math.min(4,3,6,8));//3
+// console.log(Math.max(4,3,6,8));//8
+
+// console.log(Math.random());//0.005908680591714344
+// console.log(Math.random()*10+1);//2.1214209021409838
+// console.log((Math.random()*10)+1);//8.949311540996153
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
