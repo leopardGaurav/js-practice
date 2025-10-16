9f7dc3e5f5d2812182cf115fea70a6eb08b39fb0
gaurav kumar <gauravkumar20843@gmail.com>
2025-10-16 12:32:08 +0530

    Create 01scope.js

---
+++ scope/01scope.js
@@ -0,0 +1,35 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// console.log(a)
+// console.log(b)
+// console.log(c)
+//sabhi ke sabhi print hua 
+
+//but jab ham  if ka use karte hain then 
+// let and const ka print nahi hua aur 
+// sath me error aa gaya  but var print  ho gaya
+
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+//console.log(a)
+//console.log(b)
+//console.log(c)
+
+// 👉 let aur const ka block scope hota hai
+
+// Jab aap let ya const use karte ho if ke andar, to vo sirf us block ke andar hi accessible hote hain.
+
+// Block ka matlab hai { ... } ke andar ka area.
+
+// Isliye jab aap block ke bahar console.log(a) ya console.log(b) karte ho, to error aata hai 
+
+// var ka function scope hota hai, block scope nahi hota
+
+// var ka scope pure function (ya global) me hota hai, if block ke andar nahi rukta.
+
+// Isliye jab aapne var c = 30 kiya, to vo if block ke bahar bhi accessible raha.
