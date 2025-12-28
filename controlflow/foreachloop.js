960cf9352b7a7451c0486b27ce194a901e62606f
gaurav kumar <gauravkumar20843@gmail.com>
2025-12-28 14:39:50 +0530

    Create foreachloop.js

---
+++ controlflow/foreachloop.js
@@ -0,0 +1,21 @@
+const coding = ["js","ruby","java","python","cpp"]
+
+// coding.forEach(function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+// js
+// ruby
+// java
+// python
+// cpp
+
